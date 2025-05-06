2bb9f785ee553d0af7bd7ee37d0fec1fad5331e7
matteopet <matteopetsys@gmail.com>
2025-05-06 18:26:29 +0200

    Created settings windows

--- main.py
+++ main.py
@@ -3,72 +3,73 @@ from tkinter import ttk
 from tkinter import messagebox
 
 from PIL import Image, ImageTk
+import json
 
 from home import Home
 from weeks_log import WeeksLog
 
-# class Settings(tk.Toplevel):
-#   def __init__(self, root):
-#     super().__init__()
-#     self.title("Settings")
-#     self.minsize(500,500)
+class Settings(tk.Toplevel):
+  def __init__(self, root):
+    super().__init__()
+    self.title("Settings")
+    self.minsize(500,500)
 
-#     self.languages_available = ["English"]
-#     self.start_week_available = ["Monday", "Sunday"]
+    self.languages_available = ["English"]
+    self.start_week_available = ["Monday", "Sunday"]
 
-#     self.sidepanel_frame = None
-#     self.main_container = tk.Frame(self)
-#     self.main_container.pack(fill="both", expand=True)
-#     self.content_frame = tk.Frame(self.main_container)
-#     self.content_frame.pack(side="right", fill="both", expand=True)
+    self.sidepanel_frame = None
+    self.main_container = tk.Frame(self)
+    self.main_container.pack(fill="both", expand=True)
+    self.content_frame = tk.Frame(self.main_container)
+    self.content_frame.pack(side="right", fill="both", expand=True)
 
-#     self.run_appearance_frame()
+    self.run_appearance_frame()
   
-#   def run_sidepanel_frame(self):
-#     if self.sidepanel_frame:
-#       self.sidepanel_frame.destroy()
+  def run_sidepanel_frame(self):
+    if self.sidepanel_frame:
+      self.sidepanel_frame.destroy()
 
-#     self.color_sidepanel_frame_background = "light gray"
-#     self.sidepanel_frame = tk.Frame(self.main_container, width=150, background=self.color_sidepanel_frame_background)
-#     self.sidepanel_frame.pack(side="left", fill="y")
+    self.color_sidepanel_frame_background = "light gray"
+    self.sidepanel_frame = tk.Frame(self.main_container, width=150, background=self.color_sidepanel_frame_background)
+    self.sidepanel_frame.pack(side="left", fill="y")
 
-#     tk.Label(self.sidepanel_frame, text="Settings", font=("Arial 16 bold"), background=self.color_sidepanel_frame_background).pack(side="top", anchor="center", pady=15, padx=15)
+    tk.Label(self.sidepanel_frame, text="Settings", font=("Arial 16 bold"), background=self.color_sidepanel_frame_background).pack(side="top", anchor="center", pady=15, padx=15)
 
-#     tk.Button(self.sidepanel_frame, text="Appearance", command=lambda: self.run_appearance_frame()).pack(side="top", anchor="center", fill="x")
-#     tk.Button(self.sidepanel_frame, text="Preferences", command=lambda: self.run_preferences_frame()).pack(side="top", anchor="center", fill="x", pady=5)
-#     tk.Button(self.sidepanel_frame, text="Little help", command=lambda: self.run_donation_frame()).pack(side="top", anchor="center", fill="x")
+    tk.Button(self.sidepanel_frame, text="Appearance", command=lambda: self.run_appearance_frame()).pack(side="top", anchor="center", fill="x")
+    tk.Button(self.sidepanel_frame, text="Preferences", command=lambda: self.run_preferences_frame()).pack(side="top", anchor="center", fill="x", pady=5)
+    tk.Button(self.sidepanel_frame, text="Little help", command=lambda: self.run_donation_frame()).pack(side="top", anchor="center", fill="x")
 
-#   def clear_content_frame(self):
-#     for widget in self.content_frame.winfo_children():
-#       widget.destroy()
+  def clear_content_frame(self):
+    for widget in self.content_frame.winfo_children():
+      widget.destroy()
 
-#   def run_appearance_frame(self):
-#     self.run_sidepanel_frame()
-#     self.clear_content_frame()
+  def run_appearance_frame(self):
+    self.run_sidepanel_frame()
+    self.clear_content_frame()
 
-#     tk.Label(self.content_frame, text="Working on it...").pack(padx=15, pady=15)
+    tk.Label(self.content_frame, text="Working on it...").pack(padx=15, pady=15)
 
-#   def run_preferences_frame(self):
-#     self.run_sidepanel_frame()
-#     self.clear_content_frame()
+  def run_preferences_frame(self):
+    self.run_sidepanel_frame()
+    self.clear_content_frame()
 
-#     with open('settings.json', "r") as file:
-#       settings_data = json.load(file)
+    with open('settings.json', "r") as file:
+      settings_data = json.load(file)
 
-#     tk.Label(self.content_frame, text="Preferences", font=("Arial 18 bold")).pack(padx=15, pady=15, anchor="nw")
-#     ttk.Separator(self.content_frame, orient="horizontal").pack(fill="x")
+    tk.Label(self.content_frame, text="Preferences", font=("Arial 18 bold")).pack(padx=15, pady=15, anchor="nw")
+    ttk.Separator(self.content_frame, orient="horizontal").pack(fill="x")
 
-#     for setting_name, value in settings_data["preferences"].items():
-#       frame_setting = tk.Frame(self.content_frame)
-#       frame_setting.pack(side="top", fill="x", pady=10, padx=15, ipady=5)
+    for setting_name, value in settings_data["preferences"].items():
+      frame_setting = tk.Frame(self.content_frame)
+      frame_setting.pack(side="top", fill="x", pady=10, padx=15, ipady=5)
 
-#       tk.Label(frame_setting, text=setting_name.capitalize()).pack(side="left", padx=10)
+      tk.Label(frame_setting, text=setting_name.capitalize()).pack(side="left", padx=10)
 
-#   def run_donation_frame(self):
-#     self.run_sidepanel_frame()
-#     self.clear_content_frame()
+  def run_donation_frame(self):
+    self.run_sidepanel_frame()
+    self.clear_content_frame()
 
-#     tk.Label(self.content_frame, text="hola").pack(padx=15, pady=15)
+    tk.Label(self.content_frame, text="hola").pack(padx=15, pady=15)
 
 
 class Main(tk.Tk):
@@ -108,8 +109,7 @@ class Main(tk.Tk):
     self.mainloop()
 
   def open_settings(self):
-    # Settings(self)
-    print("TODO")
+    Settings(self)
 
   def open_help(self):
     print("TODO help")
